edc81606c048b0a7b0d3aa9dce4a04e84eda4183
Loren <lm.velasquez12@gmail.com>
2025-12-20 23:27:13 -0600

    update to FAQ

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -5,18 +5,18 @@ import { useLanguage } from '@/composables/useLanguage'
 const { language } = useLanguage()
 
 const faqEN = [
+  { question: "Are you having a boy or a girl?", answer: "Come and find out." },
   { question: "Is this coed?", answer: "Yep! We are inviting you and your partner! (We are only doing this once!)" },
   { question: "Can I bring my child?", answer: "As long as they are not coughing, sneezing, sniffling or sick in any way shape or form! As a reminder, it's a municipal building acquired to us via connections so we do need to be respectful of the space. We will also not have child activities planned." },
   { question: "Is there parking available?", answer: "There is a parking lot!" },
   { question: "Is there going to be a diaper raffle?", answer: "Yep! Bring any size packs of diapers to enter!" },
-  { question: "Can I get you something off the registry?", answer: "Sure, we also accept money for daycare! Otherwise, please include a gift receipt because it's likely we already have the item or something similar." },
   { question: "Can I get you baby clothes?", answer: "No thanks!" },
+  { question: "Can I get you something not on the registry?", answer: "Sure, we also accept money for daycare! Otherwise, please include a gift receipt because it's likely we already have the item or something similar." },
   { question: "It's just allergies. I can still come, right?", answer: "We accept your presence and celebration from afar!" },
   { question: "I am not up-to-date on vaccines, can I still come?", answer: "As long as you are not currently/potentially sick. We'll also have masks available." },
   { question: "I was definitely sick 2 weeks ago but have a lingering cough but not contagious, probably...", answer: "If it's been over 10 days since you first got sick then come but wear a mask throughout the baby shower and keep some distance just in case. Otherwise, we accept your presence and celebration from afar!" },
   { question: "I can't attend anymore. Do I tell you?", answer: "Yep, giving us a heads-up would be nice! Thanks for the courtesy!" },
   { question: "I didn't RSVP, you are still anticipating that I'm showing up right?", answer: "You are? News to me." },
-  { question: "Are you having a boy or a girl?", answer: "Come and find out." },
   { question: "What if I can't attend but still want to send a gift?", answer: "Oh thank you! You can get us something from our registry that will be delivered to our home." },
   { question: "Unrelated to the baby shower, but I have some stuff you might use for baby. How do I coordinate that with you?", answer: "Just text us! We'll let you know if we could use those items!" },
 ]
